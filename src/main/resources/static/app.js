de8af7bec7a586a5174e65eb4205d6727f998183
skyd0me <m.roesch87@web.de>
2016-07-12 09:04:08 +0200

    Update app.js

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,6 +2,7 @@ function Hello($scope, $http) {
 	//'https://spring_demo.cfapps.sap.hana.ondemand.com/incident'
     $http.get('https://mrspringtest.herokuapp.com/api/greetings').
         success(function(data) {
+        	console.log(data);
             $scope.greetings = data.greetings;
         }
 	);    
