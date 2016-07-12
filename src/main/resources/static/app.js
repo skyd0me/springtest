662626981baa4fd374fddf3e2a64edb4617b0be2
skyd0me <m.roesch87@web.de>
2016-07-12 09:02:48 +0200

    Create app.js

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,8 @@
+function Hello($scope, $http) {
+	//'https://spring_demo.cfapps.sap.hana.ondemand.com/incident'
+    $http.get('https://mrspringtest.herokuapp.com/api/greetings').
+        success(function(data) {
+            $scope.greetings = data.greetings;
+        }
+	);    
+}
